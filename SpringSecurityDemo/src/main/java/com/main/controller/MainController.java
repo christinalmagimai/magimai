4643fbf2458425b75a96b3800e81df193806feb6
christinalmagimai <49547688+christinalmagimai@users.noreply.github.com>
2019-04-16 15:11:27 +0530

    Update MainController.java

--- SpringSecurityDemo/src/main/java/com/main/controller/MainController.java
+++ SpringSecurityDemo/src/main/java/com/main/controller/MainController.java
@@ -1,5 +1,5 @@
 package com.main.controller;
-//editeddddddd
+//editeddd
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
